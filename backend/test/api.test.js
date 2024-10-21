562f38ea7cb7410e0c5a44f3132321f0ef000a5f
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2024-10-21 13:32:46 -0300

    Corrigindo testes

--- backend/test/api.test.js
+++ backend/test/api.test.js
@@ -2,54 +2,54 @@ const request = require('supertest');
 const app = require('../server');
 
 describe('API', () => {
-    // Teste para criar produtos
-    it('Deve cadastrar um produto', async () => {
+    // Teste para criar um animal
+    it('Deve cadastrar um animal', async () => {
         const response = await request(app)
-            .post('/produto')
-            .send({ nome: 'Produto Teste', preco: 100 });
+            .post('/animal')
+            .send({ nome: 'Animal Teste', especie: 'Cachorro' });
         expect(response.statusCode).toEqual(201);
-        expect(response.body).toHaveProperty('nome', 'Produto Teste');
-        expect(response.body).toHaveProperty('preco', 100);
+        expect(response.body).toHaveProperty('nome', 'Animal Teste');
+        expect(response.body).toHaveProperty('especie', 'Cachorro');
     });
 
-    // Teste para listar produtos
-    it('Deve listar produtos', async () => {
-        const response = await request(app).get('/produto');
+    // Teste para listar animais
+    it('Deve listar animais', async () => {
+        const response = await request(app).get('/animal');
         expect(response.statusCode).toEqual(200);
         expect(response.body).toBeInstanceOf(Array);
     });
 
-    // Teste para buscar produto por ID
-    it('Deve buscar um produto por ID', async () => {
-        const produto = await request(app)
-            .post('/produto')
-            .send({ nome: 'Produto Para Buscar', preco: 200 });
-        const response = await request(app).get(`/produto/${produto.body._id}`);
+    // Teste para buscar animal por ID
+    it('Deve buscar um animal por ID', async () => {
+        const animal = await request(app)
+            .post('/animal')
+            .send({ nome: 'Animal Para Buscar', especie: 'Gato' });
+        const response = await request(app).get(`/animal/${animal.body._id}`);
         expect(response.statusCode).toEqual(200);
-        expect(response.body).toHaveProperty('nome', 'Produto Para Buscar');
-        expect(response.body).toHaveProperty('preco', 200);
+        expect(response.body).toHaveProperty('nome', 'Animal Para Buscar');
+        expect(response.body).toHaveProperty('especie', 'Gato');
     });
 
-    // Teste para atualizar produto
-    it('Deve atualizar um produto', async () => {
-        const produto = await request(app)
-            .post('/produto')
-            .send({ nome: 'Produto Atualizar', preco: 300 });
+    // Teste para atualizar animal
+    it('Deve atualizar um animal', async () => {
+        const animal = await request(app)
+            .post('/animal')
+            .send({ nome: 'Animal Atualizar', especie: 'Leão' });
         const response = await request(app)
-            .put(`/produto/${produto.body._id}`)
-            .send({ nome: 'Produto Atualizado', preco: 400 });
+            .put(`/animal/${animal.body._id}`)
+            .send({ nome: 'Animal Atualizado', especie: 'Formiga' });
         expect(response.statusCode).toEqual(200);
-        expect(response.body).toHaveProperty('nome', 'Produto Atualizado');
-        expect(response.body).toHaveProperty('preco', 400);
+        expect(response.body).toHaveProperty('nome', 'Animal Atualizado');
+        expect(response.body).toHaveProperty('especie', 'Formiga');
     });
 
-    // Teste para deletar produto
-    it('Deve deletar um produto', async () => {
-        const produto = await request(app)
-            .post('/produto')
-            .send({ nome: 'Produto Deletar', preco: 500 });
-        const response = await request(app).delete(`/produto/${produto.body._id}`);
+    // Teste para deletar animal
+    it('Deve deletar um animal', async () => {
+        const animal = await request(app)
+            .post('/animal')
+            .send({ nome: 'Animal Deletar', especie: 500 });
+        const response = await request(app).delete(`/animal/${animal.body._id}`);
         expect(response.statusCode).toEqual(200);
-        expect(response.body).toHaveProperty('message', "Produto deletado com sucesso");
+        expect(response.body).toHaveProperty('message', "Animal deletado com sucesso");
     }); 
 });
