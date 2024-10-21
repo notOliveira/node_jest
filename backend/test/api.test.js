600f90f7e4072c2af1bf6e286091bf46a4616f0b
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2024-10-21 13:36:32 -0300

    Corrigindo testes com novas propriedades

--- backend/test/api.test.js
+++ backend/test/api.test.js
@@ -6,10 +6,11 @@ describe('API', () => {
     it('Deve cadastrar um animal', async () => {
         const response = await request(app)
             .post('/animal')
-            .send({ nome: 'Animal Teste', especie: 'Cachorro' });
+            .send({ nome: 'Animal Teste', especie: 'Cachorro', imagem: 'link1' });
         expect(response.statusCode).toEqual(201);
         expect(response.body).toHaveProperty('nome', 'Animal Teste');
         expect(response.body).toHaveProperty('especie', 'Cachorro');
+        expect(response.body).toHaveProperty('imagem', 'link1');
     });
 
     // Teste para listar animais
@@ -23,31 +24,33 @@ describe('API', () => {
     it('Deve buscar um animal por ID', async () => {
         const animal = await request(app)
             .post('/animal')
-            .send({ nome: 'Animal Para Buscar', especie: 'Gato' });
+            .send({ nome: 'Animal Para Buscar', especie: 'Gato', imagem: 'link2' });
         const response = await request(app).get(`/animal/${animal.body._id}`);
         expect(response.statusCode).toEqual(200);
         expect(response.body).toHaveProperty('nome', 'Animal Para Buscar');
         expect(response.body).toHaveProperty('especie', 'Gato');
+        expect(response.body).toHaveProperty('imagem', 'link2');
     });
 
     // Teste para atualizar animal
     it('Deve atualizar um animal', async () => {
         const animal = await request(app)
             .post('/animal')
-            .send({ nome: 'Animal Atualizar', especie: 'Leão' });
+            .send({ nome: 'Animal Atualizar', especie: 'Leão', imagem: 'link3' });
         const response = await request(app)
             .put(`/animal/${animal.body._id}`)
-            .send({ nome: 'Animal Atualizado', especie: 'Formiga' });
+            .send({ nome: 'Animal Atualizado', especie: 'Formiga', imagem: 'link4' });
         expect(response.statusCode).toEqual(200);
         expect(response.body).toHaveProperty('nome', 'Animal Atualizado');
         expect(response.body).toHaveProperty('especie', 'Formiga');
+        expect(response.body).toHaveProperty('imagem', 'link4');
     });
 
     // Teste para deletar animal
     it('Deve deletar um animal', async () => {
         const animal = await request(app)
             .post('/animal')
-            .send({ nome: 'Animal Deletar', especie: 500 });
+            .send({ nome: 'Animal Deletar', especie: 'Quati', imagem: 'link5' });
         const response = await request(app).delete(`/animal/${animal.body._id}`);
         expect(response.statusCode).toEqual(200);
         expect(response.body).toHaveProperty('message', "Animal deletado com sucesso");
